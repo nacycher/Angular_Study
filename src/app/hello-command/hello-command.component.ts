9a2ce15f4e3016ec2ab6041a12a6150ab87dfd7b
linbo <nacycher@163,com>
2021-08-24 23:13:52 +0800

    ng指令_1

--- src/app/hello-command/hello-command.component.ts
+++ src/app/hello-command/hello-command.component.ts
@@ -6,7 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hello-command.component.scss']
 })
 export class HelloCommandComponent implements OnInit {
-  citys = ["北京","上海","成都","曹县"];
+  citys = ["北京", "上海", "成都", "曹县"];
+  people = [
+    {id:1, name:"man"},
+    {id:2, name:"luoQiang"},
+    {id:3, name:"Vincent"}
+  ];
+  charScore = "A";
 
   constructor() { }
 
